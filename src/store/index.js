7dcf233508798a6601fffaf08999a5750443d579
luiscadari <luiscadari@icloud.com>
2025-07-22 07:55:54 -0300

    :sparkles: feat: persist status

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,17 @@ import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/root.reducer';
 import rootSaga from './modules/root.saga';
+import persistedReducers from './modules/redux.persist';
+import { persistStore } from 'redux-persist';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducers(rootReducer),
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
 });
 sagaMiddleware.run(rootSaga);
 
+export const persistor = persistStore(store);
 export default store;
